604cdb4ee8c26c6a4d7fff84b558093e7d2e2cc4
piet2b <piet2bezerra+dev@gmail.com>
2023-08-21 21:32:35 -0300

    exercicios

---
+++ 4.py
@@ -0,0 +1,5 @@
+'''Faça um Programa que converta metros para centímetros.'''
+
+numero1 = float(input("Digite um número: "))
+conversao = numero1*100
+print("o valor convertido eh", conversao)
---
+++ 5.py
@@ -0,0 +1,9 @@
+'''Faça um Programa que calcule a área de um quadrado, 
+em seguida mostre o dobro desta área para o usuário.'''
+
+lado = float(input("Digite o tamanho do lado do quadrado: "))
+area = lado ** 2
+dobro_area = 2 * area
+
+print("A área do quadrado é:", area)
+print("O dobro da área do quadrado é:", dobro_area)
