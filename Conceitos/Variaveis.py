0ecd3e9068de5568a852cdc107b04c950275d5af
piet2b <piet2bezerra+dev@gmail.com>
2023-08-28 15:14:11 -0300

    variables

---
+++ Conceitos/Variaveis.py
@@ -0,0 +1,7 @@
+chacter_name = "Jhon"
+chacter_age = "40"
+
+print("There once was a man named " + chacter_name + ",")
+print("he was " + chacter_age + " years old.")
+print("He liked being " + chacter_name + ",")
+print("But he didnt like being " + chacter_age + ".")
