5335d9b174cb97930795f0b3e11d53c4f03ad640
piet2b <piet2bezerra+dev@gmail.com>
2023-12-13 10:02:42 -0300

    Flask

---
+++ Flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
